65219a1693983bcb065892c6382e2bf3943c60e7
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2023-05-10 16:17:24 +0530

    for vercel

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r8i%ijlu543exs6$d)lqt7=-mjek9w=mdjng@lneri=eh+fd0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
